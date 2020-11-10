465d574db905aa4e3c633cc4fe3110a19b337b34
saraoe <sara.m.oestergaard@gmail.com>
2020-11-10 21:25:39 +0100

    pp_n changes

--- analysis1.r
+++ analysis1.r
@@ -231,14 +231,14 @@ do_analyses_1 <- function() {
               pp_u <- this_chain_df$pp_u
               pp_sig <- this_chain_df$pp_sig
               
-              pp_n <<- c(pp_n, nrow(chain_df$pp_u))
+              pp_n <<- c(pp_n, nrow(this_chain_df))
             } else{
               pp <- kalman(mean = this_chain_df$pp_u, sd = this_chain_df$pp_sig)
               
               pp_u <- pp[,1]
               pp_sig <- pp[,2]
               
-              pp_n <<- c(pp_n, nrow(this_chain_df$pp_u))
+              pp_n <<- c(pp_n, nrow(this_chain_df))
             }
             
           }
@@ -358,14 +358,14 @@ do_analyses_1 <- function() {
               pp_u <- this_chain_df$pp_u
               pp_sig <- this_chain_df$pp_sig
               
-              pp_n <<- c(pp_n, nrow(chain_df$pp_u))
+              pp_n <<- c(pp_n, nrow(this_chain_df))
             } else{
               pp <- kalman(mean = this_chain_df$pp_u, sd = this_chain_df$pp_sig)
               
               pp_u <- pp[,1]
               pp_sig <- pp[,2]
               
-              pp_n <<- c(pp_n, nrow(this__chain_df$pp_u))
+              pp_n <<- c(pp_n, nrow(this_chain_df))
             }
           }
         }
